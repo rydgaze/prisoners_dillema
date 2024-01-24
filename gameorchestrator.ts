70dc9301896bc45ecf35f5ed423e94a09906b47a
Anis Abdul <anismo@gmail.com>
2024-01-23 20:31:12 -0600

    Update gameorchestrator.ts
    Error handling

--- gameorchestrator.ts
+++ gameorchestrator.ts
@@ -1,28 +1,85 @@
-import { StrategySim } from "./StrategySim"
+import { it } from "node:test"
+import { Action, StrategyData } from "./Strategy"
 
-const config = require('config')
+function calcScore(leftAction: Action, rightAction: Action) {
 
-const maxIterations = config.get('Execution.controls.iteration')
-const strategies = config.get('Execution.strategies')
+    let leftScore = 0
+    let rightScore = 0
+    // Rules
+    // If Both are COOPERATE = Each gets 3
+    // If One is COOPERATE and other is DEFECT = The DEFECT gets 5 and COOPERATE gets 0
+    // If Both are DEFECT = Each gets 1
 
-console.log('Testing Strategies', strategies)
-console.log('Iterations', maxIterations)
-
-let scores  = []
-
-async function executeStrategies() {
-    for (let left of strategies) {
-        let score = 0
-        for (let right of strategies) {
-            score += await StrategySim(maxIterations, left, right)
-        }
-        scores.push({ strategy: left, score: score })
+    if (leftAction == Action.COOPERATE && rightAction == Action.COOPERATE) {
+        leftScore = 3
+        rightScore = 3
+    } else if (leftAction == Action.COOPERATE && rightAction == Action.DEFECT) {
+        leftScore = 0
+        rightScore = 5
+    } else if (leftAction == Action.DEFECT && rightAction == Action.COOPERATE) {
+        leftScore = 5
+        rightScore = 0
+    } else if (leftAction == Action.DEFECT && rightAction == Action.DEFECT) {
+        leftScore = 1
+        rightScore = 1
     }
-    scores.sort((a, b) => b.score - a.score);
-    console.log('Results',scores)
 
-    
+    return { leftScore, rightScore }
 }
 
-executeStrategies()
+export async function StrategySim(iterations: number,
+    leftStrategyName: string,
+    rightStrategyName: string) {
 
+    //console.log('Simulating iterations:', iterations, ' [', leftStrategyName, ' VS ', rightStrategyName, ']')
+
+    // Import both the strategies from files
+    let l = null
+    let r = null
+    try {
+        l = await import("./Strategies/" + leftStrategyName)
+        r = await import("./Strategies/" + rightStrategyName)
+
+    } catch (e) {
+        console.log("FATAL:",e.message)
+        process.exit()
+    }
+
+    let leftStrategy = new l.default()
+    let rightStrategy = new r.default()
+
+    let leftData = new StrategyData()
+    let rightData = new StrategyData()
+
+    let leftTotal = 0
+    let rightTotal = 0
+
+    leftData.myActions = []
+    rightData.myActions = []
+    leftData.opponentActions = []
+    rightData.opponentActions = []
+
+    for (let iteration = 0; iteration < iterations; iteration++) {
+        leftData.iteration = iteration
+        rightData.iteration = iteration
+        let leftAction = leftStrategy.getAction(leftData)
+        let rightAction = rightStrategy.getAction(rightData)
+
+        leftData.myActions.push(leftAction)
+        leftData.opponentActions.push(rightAction)
+
+        rightData.myActions.push(rightAction)
+        rightData.opponentActions.push(leftAction)
+
+        let score = calcScore(leftAction, rightAction)
+        //console.log('ACTION: ',leftStrategy.name(), ' : ', leftAction , ', ', rightStrategy.name(), ' : ', rightAction)
+
+        leftTotal += score.leftScore
+        rightTotal += score.rightScore
+
+    }
+
+    //console.log('AVERAGE: ',leftStrategy.name(), ' : ', leftTotal/iterations , ', ', rightStrategy.name(), ' : ', rightTotal/iterations)
+    return leftTotal
+
+}
